51dc83fc585fe3b7dc47ef7123b1f4569771257e
chen <38033919@qq.com>
2017-04-07 15:16:30 +0800

    Initial commit

---
+++ src/main/java/com/chen/controller/HelloWorldController.java
@@ -0,0 +1,18 @@
+package com.chen.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * Created by chen on 17/4/7.
+ */
+@Controller
+public class HelloWorldController {
+    @RequestMapping("/attendance/getLocation")
+    @ResponseBody
+    public String getHelloworld() {
+        return "helloworld";
+    }
+
+}
