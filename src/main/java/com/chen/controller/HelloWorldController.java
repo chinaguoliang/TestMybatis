6b99d143b227e2d6bd5b3a43b6861a21202c8b05
chen <38033919@qq.com>
2017-04-07 15:52:23 +0800

    增加文档生成插件

--- src/main/java/com/chen/controller/HelloWorldController.java
+++ src/main/java/com/chen/controller/HelloWorldController.java
@@ -1,5 +1,7 @@
 package com.chen.controller;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -7,12 +9,22 @@ import org.springframework.web.bind.annotation.ResponseBody;
 /**
  * Created by chen on 17/4/7.
  */
+@Api(value = "测试文档",description="测试文档是否能生成")
 @Controller
 public class HelloWorldController {
+    @ApiOperation(value="测试Swagger2", notes=" name 32 位数")
     @RequestMapping("/attendance/getLocation")
     @ResponseBody
     public String getHelloworld() {
         return "helloworld haha chen";
+    }
+
+
+    @ApiOperation(value="获取用户数据", notes="获取所有登录用户到数据")
+    @RequestMapping("/ok")
+    @ResponseBody
+    public String getHelloworld2() {
+        return "ok";
     }
 
 }
