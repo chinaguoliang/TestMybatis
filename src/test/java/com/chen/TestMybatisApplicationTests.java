46b50a6c573cdcf1ed489bcceabc9adc565c7691
chen <38033919@qq.com>
2017-05-14 02:34:32 +0800

    修改测试类

--- src/test/java/com/chen/TestMybatisApplicationTests.java
+++ src/test/java/com/chen/TestMybatisApplicationTests.java
@@ -11,11 +11,5 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class TestMybatisApplicationTests {
-	@Autowired
-	private UserInfoMapper userInfoMapper;
-	@Test
-	public void contextLoads() {
-		UserInfo ui = userInfoMapper.selectByPrimaryKey(10);
-		System.out.println("------------------>" + ui.getId());
-	}
+
 }
